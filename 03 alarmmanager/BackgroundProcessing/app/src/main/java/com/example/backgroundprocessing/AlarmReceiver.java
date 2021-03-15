7153a5b82128c71e1702ab9c01aeadd78e3342c3
Joyce Echessa <jokhessa@yahoo.com>
2021-03-15 22:49:00 +0300

    Include completed project for the AlarmManager section

---
+++ 03 alarmmanager/BackgroundProcessing/app/src/main/java/com/example/backgroundprocessing/AlarmReceiver.java
@@ -0,0 +1,20 @@
+package com.example.backgroundprocessing;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private static final String TAG = BroadcastReceiver.class.getName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Log.d(TAG, "onReceive: Running recurring task");
+
+    }
+}
---
+++ 03 alarmmanager/BackgroundProcessing/app/src/main/java/com/example/backgroundprocessing/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.backgroundprocessing;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AlarmManager manager;
+    private PendingIntent pendingIntent;
+    private static final int REQ_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SwitchCompat alarmSwitch = findViewById(R.id.alarmSwitch);
+
+        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+
+        boolean alarmOn = (PendingIntent.getBroadcast(this, REQ_CODE, alarmIntent, PendingIntent.FLAG_NO_CREATE) != null);
+        alarmSwitch.setChecked(alarmOn);
+
+        manager = (AlarmManager)getSystemService(ALARM_SERVICE);
+
+        alarmSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String message;
+                if (isChecked) {
+                    pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), REQ_CODE, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 60000, pendingIntent);
+                    message = "Alarm On";
+                } else {
+                    if (pendingIntent == null) {
+                        pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), REQ_CODE, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    }
+                    manager.cancel(pendingIntent);
+                    pendingIntent.cancel();
+                    message = "Alarm Off";
+                }
+                // Show a toast with message.
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
